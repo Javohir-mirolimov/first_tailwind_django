3f6ce851ab0a4a5503aa19275e747c6321a2a666
Zulf1qor <zsharibjonov@gmail.com>
2024-05-18 17:04:27 +0500

    Zulfiqor

--- main/models.py
+++ main/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Banner(models.Model):
+    title = models.CharField(max_length=155)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='Banner-image/')
+    teg = models.ManyToManyField(to='Teg')
+
+
+
+class Teg(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class Activ_work(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='logo_image/')
+
+
+class Frilanser(models.Model):
+    name = models.CharField(max_length=25)
+    tecnology = models.CharField(max_length=25)
+    proects = models.IntegerField(default=0)
+    star = models.FloatField(default=0)
+    image = models.ImageField(upload_to='Image_frilanser/')
+
+
+class Worktap(models.Model):
+    image = models.ImageField(upload_to='worktap_image/')
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+
+
+class Work(models.Model):
+    image = models.ImageField(upload_to='worktap_image/')
+    title = models.CharField(max_length=25)
+
+
+class Info(models.Model):
+    logo = models.ImageField(upload_to='logo_image/')
+    facebook = models.CharField(max_length=255)
+    twitter = models.CharField(max_length=255)
+    instagramm = models.CharField(max_length=255)
+
