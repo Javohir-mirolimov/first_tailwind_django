62d7a6c0007ae1c238fd90d5e986890423fd6d55
Javohir-mirolimov <githup@gmail.com>
2024-05-23 17:42:54 +0500

    javohir

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('', home, name="home_urls")
+    path('', home, name="home_url"),
+    path('birja-url', birja, name="birja_url"),
+    path('profil-url', birja, name="profil_url"),
+
 
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 def home(requests):
 
     return render(requests,'index.html')
+
+
+def birja(requests):
+
+    return render(requests,'birja.html')
+
+
+
+def profil(requests):
+
+    return render(requests,'profil.html')
